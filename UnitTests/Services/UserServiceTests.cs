b9b1951664714807e0bdfde216cce8eb8fecf91a
Serfin <gracjanbrylka@gmail.com>
2019-03-21 01:11:37 +0100

    Cleaned up unit tests

--- UnitTests/Services/UserServiceTests.cs
+++ UnitTests/Services/UserServiceTests.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using ExileRota.Core.Domain;
 using ExileRota.Core.Repositories;
+using ExileRota.Infrastructure.DTO;
 using ExileRota.Infrastructure.Services;
 using Moq;
 using NUnit.Framework;
@@ -14,18 +15,33 @@ namespace ExileRota.UnitTests.Services
     public class UserServiceTests
     {
         [Test]
+        public async Task calling_get_all_async_should_return_all_users()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+            var encrypterMock = new Mock<IEncrypter>();
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object, encrypterMock.Object);
+            IEnumerable<User> user = new List<User> { It.IsAny<User>() };
+            IEnumerable<UserDto> userDto = new List<UserDto> { It.IsAny<UserDto>() };
+
+            userRepositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(user);
+            mapperMock.Setup(x => x.Map<IEnumerable<User>, IEnumerable<UserDto>>(user)).Returns(userDto);
+            var users = await userService.GetAllAsync();
+
+            Assert.IsNotEmpty(users);
+        }
+        [Test]
         public async Task calling_register_async_should_invoke_user_repository_add_assync()
         {
             var userRepositoryMock = new Mock<IUserRepository>();
             var mapperMock = new Mock<IMapper>();
             var encrypterMock = new Mock<IEncrypter>();
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object, encrypterMock.Object);
 
             encrypterMock.Setup(x => x.GetSalt(It.IsAny<string>())).Returns("hash123");
             encrypterMock.Setup(x => x.GetHash(It.IsAny<string>(), It.IsAny<string>())).Returns("salt123");
-
-            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object, encrypterMock.Object);
-
             await userService.RegisterAsync(Guid.NewGuid(), "user1", "password123qwe", "user1@gmail.com", "ign1", "user");
+
             userRepositoryMock.Verify(x => x.AddAsync(It.IsAny<User>()), Times.Once);
         }
 
